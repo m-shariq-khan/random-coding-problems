ff1fc350775eefffc13b7a45c2185f2c1a02a70f
Mohd. Shariq Khan <sharz@Mohds-MacBook-Pro.local>
2020-05-23 17:31:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Random coding problems:
+
+Use the basic-template.cpp and replace the solve() to execute a solution.
---
+++ basic-template.cpp
@@ -0,0 +1,23 @@
+#include <bits/std++.h> 
+
+#define ll long long   
+
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        solve();
+    }
+    return 0;
+}   
